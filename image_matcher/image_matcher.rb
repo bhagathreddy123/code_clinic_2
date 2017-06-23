f986ec77fd761243d723b4f7ac86083bed5254ee
bhagath <bhagath@getcustomexposure.com>
2017-06-23 07:19:56 +0530

    matcher image_matcher classes implementation and image_matcher_strategies module file defined

---
+++ image_matcher/image_matcher.rb
@@ -0,0 +1,123 @@
+require 'RMagick'; include Magick
+require 'benchmark'
+ 
+require_relative('image_matcher_strategies')
+ 
+class ImageMatcher
+	attr_reader :search_image, :template_image
+	attr_reader :match_x, :match_y, :benchmark
+	attr_reader :search_cols,:search_rows
+	attr_accessor :strategy, :verbose,:highlight_match, :fuzz
+	 
+	@@strategies = {}
+	 include ImageMatcherStrategies
+
+	def initialize(options={})
+	search_image = options[:search_image]
+	template_image = options[:template_image]
+	@strategy = options[:strategy]
+	@verbose = options[:verbose] === false ? False : true
+	@highlight_match = options[:verbose] || false
+	@fuzz = options[:fuzz] || 0.0
+	end
+	 
+	def search_image=(filepath)
+	@search_image = read_image(filepath)
+	@search_cols = search_image.columns
+	@search_rows = search_image.rows
+		return search_image
+	end
+	 
+	def template_image=(file_path)
+	@template_image = read_image(file_path)
+	end
+ 
+	def has_match?
+		!match_x.nil? && !match_y.nil?
+	end
+	 
+	 
+	def match_result
+		[match_x,match_y]
+	end
+	 
+	def clear!
+		@match_x = nil
+		@match_y = nil
+	end
+	 
+	def match!
+		clear!
+		tighten_search_area
+		@benchmark = Benchmark.measure do
+			send(strategy_method)
+		end
+		save_match_file if highlight_match
+		return has_match?
+	end
+ 
+	private
+ 
+	def read_image(filename)
+		if filename
+			image = Magick:: Image.read(filename).first
+			return image
+		end
+	end
+	 
+	def tighten_search_area
+		@search_cols = search_image.columns – template_image.columns
+		@search_rows = search_image.rows – template_image.rows
+	end
+	 
+	def add_fuzz_to_images
+	# need to implement later
+	end
+ 
+	def strategy_method
+		if @@strategies.size == 0
+			puts "no match strategies defined."
+			exit
+		end
+		strategy_method = @@strategies[strategy]
+		raise "Invalid match strategy " if strategy_method.nil?
+		return strategy_method
+	end
+	 
+	def match_result=(array)
+		if array && array.is_a?(Array)
+			@match_x,@match_y = array
+		end
+	end
+ 
+	def save_match_file
+		if match_x && match_y
+			end_x = match_x + template_image.columns
+			end_y = match_y + template_image.rows
+	 
+			area = Magick::Draw.new
+			area.fill('none')
+			area.stroke('red')
+			area.stroke_width(3)
+			area.rectangle(match_x,match_y,end_x,end_y)
+			area.draw(search_image)
+			search_image.write(matchfile)
+	 
+		else
+			raise "No Match found"
+		end
+	end
+ 
+ 
+	def matchfile
+		if search_image
+			name_parts = search_image.filename.split('.')
+			ext = name_parts.pop
+			name = name_parts.join('.')
+			return "#{name}_match.#{ext}"
+		else
+			return "no_search_image.png"
+		end
+	end
+ 
+end
---
+++ match.rb
@@ -0,0 +1,37 @@
+require_relative('image_matcher/image_matcher.rb')
+ 
+this_dir = File.expand_path(File.dirname(_FILE_))
+image_dir = File.join(this_dir,'images')
+ 
+ 
+image_tests = [
+ 
+[ 'haystack.png', 'needle.png'],
+ 
+]
+image_tests.each_with_index do |test_set,i|
+  puts
+  puts "----Image Set #{i+1} -----"
+  puts "Does '#{test_set[0]}' contain '#{test_set[1]}'?"
+ 
+search_image_path = File.join(image_dir,test_set[0])
+template_image_path = File.join(image_dir,test_set[1])
+ 
+im = ImageMatcher.new
+im.search_image = search_image_path
+im.template_image = template_image_path
+im.verbose = true
+im.strategy = 'full'
+im.fuzz = 0.0
+im.highlight_match = true
+im.match!
+ 
+if im.has_match?
+     puts "\n Yes Matches at : " + im.match_result.join("/")
+else
+       puts "\n No match"
+end
+puts "\n Search time using '#{im.strategy}': #{im.benchmark.total} seconds \n"
+puts "_" * 24
+puts
+end
